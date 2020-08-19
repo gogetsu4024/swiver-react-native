ba24f5481aeffd2143e230a9de8d843e92193713
gogetsu4024 <42039041+gogetsu4024@users.noreply.github.com>
2020-08-19 02:19:09 +0200

    rename app

--- android/app/src/main/java/com/swiver/MainActivity.java
+++ android/app/src/main/java/com/swiver/MainActivity.java
@@ -1,4 +1,4 @@
-package com.boilerplate;
+package com.swiver;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "Boilerplate";
+        return "swiver";
     }
 
     @Override
--- android/app/src/main/java/com/swiver/MainApplication.java
+++ android/app/src/main/java/com/swiver/MainApplication.java
@@ -1,4 +1,4 @@
-package com.boilerplate;
+package com.swiver;
 
 import android.app.Application;
 import android.content.Context;
